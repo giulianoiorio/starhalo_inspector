3a9f23e88478494171a8705a5d632ce4bd159b71
Giuliano Iorio <iogiul@laml46.ast.private.cam.ac.uk>
2018-04-23 17:47:26 +0100

    change name makehalo script

--- setup.py
+++ setup.py
@@ -14,25 +14,16 @@ ext_modules=cythonize(cy_ext)
 
 setup(
 		name='pycam',
-		version='2.7.0',
+		version='2.7.1',
 		author='Giuliano Iorio',
 		author_email='',
 		url='',
 		packages=['pycam'],
-        scripts=['pycam/script/makehalo.py'],
+        scripts=['pycam/script/makehalo'],
         ext_modules=ext_modules
 	)
 
 
-#script
-if os.getuid() == 0:
-    file='pycam/script/makehalo.py'
-    st = os.stat(file)
-    os.chmod('pycam/script/makehalo.py', st.st_mode | stat.S_IEXEC)
-    shutil.copy(file,'/usr/bin/makehalo')
-else:
-    print("**WARNING: This program is not run as sudo, the script files will be not moved to /urs/bin**")
-	
 	
 
 try:
